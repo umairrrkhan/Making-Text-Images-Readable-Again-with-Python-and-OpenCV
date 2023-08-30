126863d87b71e4e1cd76441a2bbb318fd65025ba
Umair khan <umair64066@gmail.com>
2023-08-30 21:03:13 +0530

    Making Text Images Readable Again with Python and OpenCV
    Making Text Images Readable Again with Python and OpenCV

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+# Load the image
+img = cv.imread('fourth.jpg')  # Make sure to provide the correct image file extension
+
+# Convert the image to grayscale
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Apply a fixed threshold
+_, result = cv.threshold(img_gray, 25, 255, cv.THRESH_BINARY)
+
+# Apply adaptive thresholding
+adaptive = cv.adaptiveThreshold(img_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 81, 4)
+
+# Display the images
+cv.imshow('Original Image', img)
+cv.imshow('Thresholded Image', result)
+cv.imshow('Adaptive Thresholded Image', adaptive)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
